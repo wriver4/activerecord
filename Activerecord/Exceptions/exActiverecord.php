b8b8c2f693e80f3c3ca6fc4ab03a7c4de6f89816
wriver4 <wriver4@gmail.com>
2016-05-05 20:50:28 -0400

    fix unintentional delete

---
+++ Activerecord/Exceptions/exActiverecord.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Activerecord\Exceptions;
+
+/**
+ * Summary of file Activerecord.
+ *
+ * Description of file Activerecord.
+ *
+ *
+ * @license
+ *
+ * @copyright
+ *
+ * @version
+ *
+ * @author mark weisser <mark at whizbangdevelopers.com>
+ */
+
+/**
+  Generic base Exceptions for all Activerecord specific errors.
+ *
+ * @package Activerecord
+ */
+class exActiverecord
+        extends \Exceptions
+{
+
+}
