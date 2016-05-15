d2712a86dea4109b876cc5ffcd2bc20f3a4c88ce
wriver4 <wriver4@gmail.com>
2016-05-15 02:41:24 -0400

    Upload

---
+++ Test/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+// TODO: check include path
+ini_set('include_path',
+        ini_get('include_path').PATH_SEPARATOR.dirname(__FILE__).'/../../../xampp/php/ext'.PATH_SEPARATOR.dirname(__FILE__).'/../../../xampp/php');
+
+// put your code here
+
