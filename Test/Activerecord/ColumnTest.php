4252edd16b243e74ea7dfd1f455d64de131d1698
wriver4 <wriver4@gmail.com>
2016-05-14 23:41:09 -0400

    repair

---
+++ Test/Activerecord/ColumnTest.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ColumnTest
+ *
+ * @author mark weisser <mark at whizbangdevelopers.com>
+ */
+class ColumnTest
+        extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var \RemoteWebDriver
+     */
+    protected $webDriver;
+
+    public function setUp()
+    {
+        $capabilities = array(
+            \WebDriverCapabilityType::BROWSER_NAME => 'firefox');
+        $this->webDriver = RemoteWebDriver::create('http://localhost:4444/wd/hub',
+                        $capabilities);
+    }
+
+    public function tearDown()
+    {
+        $this->webDriver->close();
+    }
+
+    protected $url = 'http://www.netbeans.org/';
+
+    public function testSimple()
+    {
+        $this->webDriver->get($this->url);
+        // checking that page title contains word 'NetBeans'
+        $this->assertContains('NetBeans', $this->webDriver->getTitle());
+    }
+
+}
