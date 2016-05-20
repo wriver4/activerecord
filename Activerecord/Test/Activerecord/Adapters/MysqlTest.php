82f115ec5aea856cdc48ceb0f27d3db15a0ebcbd
wriver4 <wriver4@gmail.com>
2016-05-20 09:39:02 -0400

    Begin Changes to match new format

--- Activerecord/Test/Activerecord/Adapters/MysqlTest.php
+++ Activerecord/Test/Activerecord/Adapters/MysqlTest.php
@@ -7,23 +7,56 @@
 
 namespace Activerecord\Test\Activerecord\Adapters;
 
+use Activerecord\Column;
+use Activerecord\Connection;
+use Activerecord\Config;
+use Activerecord\Test\Helpers\AdapterTest;
+
 /**
  * Description of MysqlTest
  *
  * @author mark weisser <mark at whizbangdevelopers.com>
  */
 class MysqlTest
-        extends \PHPUnit_Framework_TestCase
+        extends AdapterTest
 {
 
-    public function setUp()
+    public function setUp($connection_name = null)
     {
-
+        parent::setUp('mysql');
     }
 
     public function tearDown()
     {
 
+    }
+
+    public function testEnum()
+    {
+        $author_columns = $this->conn->columns('authors');
+        $this->assertEquals('enum', $author_columns['some_enum']->raw_type);
+        $this->assertEquals(Column::STRING, $author_columns['some_enum']->type);
+        $this->assertSame(null, $author_columns['some_enum']->length);
+    }
+
+    public function testSetCharset()
+    {
+        $connection_string = Config::instance()->getConnection($this->connection_name);
+        $conn = Connection::instance($connection_string.'?charset=utf8');
+        $this->assertEquals('SET NAMES ?', $conn->last_query);
+    }
+
+    public function testLimitWithNullOffsetDoesNotContainOffset()
+    {
+        $ret = [];
+        $sql = 'SELECT * FROM authors ORDER BY name ASC';
+        $this->conn->queryAndFetch($this->conn->limit($sql, null, 1),
+                function($row) use (&$ret)
+        {
+            $ret[] = $row;
+        });
+
+        $this->assertTrue(\strpos($this->conn->last_query, 'LIMIT 1') !== false);
     }
 
 }
