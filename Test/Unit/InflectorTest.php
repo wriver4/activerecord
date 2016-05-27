c29d33f54b7f4f6b18fdfacb94c759815b0f5152
wriver4 <wriver4@gmail.com>
2016-05-27 17:35:50 -0400

    Add All Passing Tests

---
+++ Test/Unit/InflectorTest.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Test;
+
+use Activerecord\Inflector;
+
+/**
+ * Description of InflectorTest
+ *
+ * @author mark weisser <mark at whizbangdevelopers.com>
+ */
+class InflectorTest
+        extends \PHPUnit_Framework_TestCase
+{
+
+    public function setUp()
+    {
+        $this->inflector = Inflector::instance();
+    }
+
+    public function tearDown()
+    {
+
+    }
+
+    public function testUnderscorify()
+    {
+        $this->assertEquals('rm__name__bob',
+                $this->inflector->variablize('rm--name  bob'));
+        $this->assertEquals('One_Two_Three',
+                $this->inflector->underscorify('OneTwoThree'));
+    }
+
+    public function testTableize()
+    {
+        $this->assertEquals('angry_people',
+                $this->inflector->tableize('AngryPerson'));
+        $this->assertEquals('my_sqls', $this->inflector->tableize('MySQL'));
+    }
+
+}
